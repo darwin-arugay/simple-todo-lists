8f7799fdeb7228d7ed445f3dbdd058c9b5663d4c
Dars <darwin.arugay.bsit@gmail.com>
2024-02-08 22:36:00 +0800

    feature: implement todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>TODO</h1>
-}
+import './styles.css'
+import Todo from './components/todo-list/Todo'
+import Header from './components/Header'
+import { TodoProvider } from './context/TodoContext'
 
-export default App
+export default function App() {
+  return (
+    <main>
+      <TodoProvider>
+        <Header />
+        <Todo />
+      </TodoProvider>
+    </main>
+  )
+}

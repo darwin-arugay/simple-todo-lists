5616efb6b42b9495e6742ca541ac9c701ca1a547
Dars <darwin.arugay.bsit@gmail.com>
2024-02-10 18:40:11 +0800

    refactor: styles the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  padding: 1.5rem 1rem;
+  background-color: #f8f8f8;
+  border-bottom: 2px solid #ddd;
+  border-radius: 3px;
+`
+
+const StyledHeading = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #2a80e5;
+  margin: 0;
+`
+
+const StyledSubHeading = styled.p`
+  font-size: 0.875rem;
+  margin: 0;
+  color: #777;
+  text-align: center;
+`
 
 const Header = () => {
   return (
-    <header>
-      <h2>My Todo List</h2>
-      <p>This is my todo list, add more todos</p>
-    </header>
+    <StyledHeader>
+      <StyledHeading>Your Daily Agenda</StyledHeading>
+      <StyledSubHeading>
+        Boost your productivity by keeping track of your tasks in one place.
+      </StyledSubHeading>
+    </StyledHeader>
   )
 }
 

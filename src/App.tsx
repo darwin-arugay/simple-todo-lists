ba734b23af836a3971fb8863d387bda4c720fe50
Dars <darwin.arugay.bsit@gmail.com>
2024-02-10 18:42:45 +0800

    refactor: move header component in todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './styles.css'
 import Todo from './components/todo-list/Todo'
-import Header from './components/Header'
 import { TodoProvider } from './context/TodoContext'
 
 export default function App() {
   return (
     <main>
       <TodoProvider>
-        <Header />
         <Todo />
       </TodoProvider>
     </main>

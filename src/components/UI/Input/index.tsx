bd20fb44d7e34595e1803509a1710c98b6e54cd1
Dars <darwin.arugay.bsit@gmail.com>
2024-02-12 21:33:59 +0800

    refactor: input component accept fullWidth props

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,9 +1,15 @@
 import { styled } from 'styled-components'
 
-export const Input = styled.input<{ $isError?: boolean }>`
+export const Input = styled.input<{ $isError?: boolean; $fullWidth?: boolean }>`
   padding: 0.5rem 1rem;
   border: 1.5px solid ${(props) => (props.$isError ? 'red' : '#ccc')};
   border-radius: 4px;
+
+  ${(props) =>
+    props.$fullWidth &&
+    `
+    width: 100%;
+  `}
 
   &:focus {
     border: 1.5px solid ${(props) => (props.$isError ? 'red' : '#ccc')};

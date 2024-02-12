d699dc85771bbeef74a07e03cb033dd071ba9129
Dars <darwin.arugay.bsit@gmail.com>
2024-02-12 21:35:27 +0800

    refactor: remove reduncdant code in button component

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -2,22 +2,53 @@ import styled from 'styled-components'
 
 type TVariant = 'default' | 'primary' | 'outlined'
 
-export const Button = styled.button<{ $variant?: TVariant }>`
+export const Button = styled.button<{ $variant?: TVariant; $fullWidth?: boolean }>`
   height: 35px;
-  color: ${(props) => (props.$variant === 'primary' ? '#fff' : '#0cafff')};
   padding: 0.5rem 1rem;
-  background-color: ${(props) => (props.$variant === 'primary' ? '#0cafff' : 'transparent')};
-  border: 1px solid ${(props) => (props.$variant === 'outlined' ? '#0cafff' : 'transparent')};
-  cursor: pointer;
   font-size: 0.875rem;
+  border: 1px solid transparent;
   border-radius: 4px;
+  cursor: pointer;
+  transition:
+    background-color 0.3s,
+    color 0.3s,
+    border-color 0.3s;
 
-  &:hover {
-    border: 1px solid #0cafff;
-    background-color: ${(props) => (props.$variant === 'primary' ? '#0c99df' : '')};
-    color: ${(props) => (props.$variant === 'primary' ? '#fff' : '#0cafff')};
-  }
+  ${(props) =>
+    props.$fullWidth &&
+    `
+    width: 100%;
+    `}
 
+  /* Default styles */
+  color: var(--color-primary-text);
+  background-color: transparent;
+
+  /* Variant styles */
+  ${(props) =>
+    props.$variant === 'primary' &&
+    `
+    color: #fff;
+    background-color: var(--color-primary-text);
+
+    &:hover {
+      background-color: #0c99df;
+    }
+  `}
+
+  ${(props) =>
+    props.$variant === 'outlined' &&
+    `
+    color: var(--color-primary-text);
+    border: 1px solid var(--color-primary-text);
+
+    &:hover {
+      background-color: var(--color-primary-text);
+      color: #fff;
+    }
+  `}
+
+  /* Disabled styles */
   &:disabled {
     background-color: #7fd5ff;
     color: #fff;
